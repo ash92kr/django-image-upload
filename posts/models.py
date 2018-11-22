b46a2ac81e2b7b60bd4241c1d4daccad07a9f660
ash <ash92kr@naver.com>
2018-11-22 05:33:31 +0000

    ADD CRUD for Post

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
     image = models.ImageField()
     content = models.TextField()
+    
+    def get_absolute_url(self):  # self = 하나의 게시글이 생성됨
+        return reverse('posts:detail', kwargs={'pk':self.pk})
+    # post가 생성되거나 수정될 때 이를 만들어 어떤 상황이든 self.pk로 이동함, 주소에 번호가 들어가므로 keyword arguments가 key: value로 들어감
+    # => /posts/1 -> 게시글을 생성하면 바로 그 게시글이 있는 사이트로 넘어간다
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
 class PostList(ListView):
     model = Post
+    
+class PostCreate(CreateView):  # CBV
+    model = Post
+    fields = ['image', 'content',]  # 리스트의 마지막은 ,를 붙일 것
+    # template_name = '.html' 사용하나 기본값은 _form.html 사용함
+    # success_url / def get_success_url
+
+class PostDetail(DetailView):
+    model = Post
+    
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['image', 'content',]
+    
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts:list')  # 게시글을 삭제하므로 원래 게시글을 불러올 수 없다
+    
